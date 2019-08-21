ae69f146dade9e419b6d8fabd118cd4abc039bd5
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-08-20 21:01:47 -0300

    Alterações no Spinner da tela de Alterar Ocorrencia

--- app/src/main/java/com/projetos/mub/AlterarOcorrencia.java
+++ app/src/main/java/com/projetos/mub/AlterarOcorrencia.java
@@ -2,16 +2,22 @@ package com.projetos.mub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AlterarOcorrencia extends AppCompatActivity {
     ImageView ImgPerfilUsuario, ImgOcorrencia;
     TextView NomeUsuario, EmailUsuario, ctLocalOcorrencia, ctImagemOcorrencia,
             ctDataOcorrencia, ctHorarioOcorrencia, ctTextoStatusOcorrencia, ctStautsOcorrencia;
     Button btAlterarDados;
+    Spinner spinnerStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,25 @@ public class AlterarOcorrencia extends AppCompatActivity {
         ctTextoStatusOcorrencia = (TextView) findViewById(R.id.ctTextoStatusOcorrencia);
         ctStautsOcorrencia = (TextView) findViewById(R.id.ctStatusOcorrencia);
         btAlterarDados = (Button) findViewById(R.id.btAlterarDados);
+        spinnerStatus = (Spinner) findViewById(R.id.spinnerStatus);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Status_Ocorrencias, R.layout.support_simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerStatus.setAdapter(adapter);
+
+        spinnerStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Toast.makeText(getBaseContext(),spinnerStatus.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
