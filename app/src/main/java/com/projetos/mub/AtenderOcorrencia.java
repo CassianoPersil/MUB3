b653b5e7a7ff7c82145636ef06e738538b38de7b
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-08-26 19:43:21 -0300

    Elaboração da tela de Atender Ocorrencia

---
+++ app/src/main/java/com/projetos/mub/AtenderOcorrencia.java
@@ -0,0 +1,13 @@
+package com.projetos.mub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtenderOcorrencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atender_ocorrencia);
+    }
+}
