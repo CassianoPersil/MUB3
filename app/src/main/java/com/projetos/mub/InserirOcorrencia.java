b096e3682f0830df031392e7cff8c2b2535dd217
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-09-01 13:31:33 -0300

    metodos para capturar data e hora

--- app/src/main/java/com/projetos/mub/InserirOcorrencia.java
+++ app/src/main/java/com/projetos/mub/InserirOcorrencia.java
@@ -1,6 +1,8 @@
 package com.projetos.mub;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -13,18 +15,29 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class InserirOcorrencia extends AppCompatActivity {
+import java.util.Calendar;
+
+public class InserirOcorrencia extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     private Object ImageViewFoto;
 
     Spinner sp;
     Button btEnviarOco, btInserirImagem;
+    TextView ctDataOco, ctHorarioOco;
     ImageButton imgOcorrencia, imgData,imgHoras;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    String amPm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,8 @@ public class InserirOcorrencia extends AppCompatActivity {
 
         sp = (Spinner) findViewById(R.id.sp);
         imgOcorrencia = (ImageButton) findViewById(R.id.imgOcorrencia);
+        ctDataOco = (TextView) findViewById(R.id.ctDataOco);
+        ctHorarioOco = (TextView) findViewById(R.id.ctHorarioOco);
         btEnviarOco = (Button) findViewById(R.id.btEnviarOco);
         imgData = (ImageButton) findViewById(R.id.imgData);
         imgHoras = (ImageButton) findViewById(R.id.imgHoras);
@@ -83,6 +98,41 @@ public class InserirOcorrencia extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //evento para chamar o relogio
+        imgHoras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(InserirOcorrencia.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        ctHorarioOco.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+
+            }
+        });
+
+        //evento para chamar o calendario
+
+        imgData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
     }
 
     //metodo para realizar a foto
@@ -91,7 +141,7 @@ public class InserirOcorrencia extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
-    //retornar o resultado
+    //retornar o resultado da camera
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == 1){
@@ -102,4 +152,24 @@ public class InserirOcorrencia extends AppCompatActivity {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+//metodo para setar a data no calendario
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String Date = dayOfMonth + "/" + month + "/" + year;
+        ctDataOco.setText(Date);
+
+    }
+
+//metodo para capturar a data
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+
+
 }
