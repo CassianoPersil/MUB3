9acc70b5740280be20e1236af9aceebf72280493
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-09-11 22:44:36 -0300

    Alteração na tela de listrar ocorrencia

--- app/src/main/java/com/projetos/mub/ListrarOcorrencias.java
+++ app/src/main/java/com/projetos/mub/ListrarOcorrencias.java
@@ -3,6 +3,9 @@ package com.projetos.mub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,6 +26,25 @@ public class ListrarOcorrencias extends AppCompatActivity {
         listrarOcorrenciaEmail = (TextView) findViewById(R.id.listrarOcorrenciaEmail);
         spListaOcorrencia = (Spinner) findViewById(R.id.spListaOcorrencia);
         reclistaOcorrencia = (RecyclerView) findViewById(R.id.recListaOcorencia);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Status_Ocorrencias, R.layout.support_simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spListaOcorrencia.setAdapter(adapter);
+
+        spListaOcorrencia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Toast.makeText(getBaseContext(),spinnerStatus.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
 
