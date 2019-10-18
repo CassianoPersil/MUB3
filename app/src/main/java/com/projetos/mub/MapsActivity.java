50f2323484f0c2a2e051e1613c455cba035bb114
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-10-18 20:07:36 -0300

    "Alterações no google maps"

--- app/src/main/java/com/projetos/mub/MapsActivity.java
+++ app/src/main/java/com/projetos/mub/MapsActivity.java
@@ -2,23 +2,30 @@ package com.projetos.mub;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getMapAsync(this);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
@@ -39,18 +46,30 @@ public class MapsActivity extends SupportMapFragment implements OnMapReadyCallba
         mMap.setOnMapClickListener(this);
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
+       /* // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-33.87365, 151.20689);
         MarkerOptions marker = new MarkerOptions();
         marker.position(sydney);
         marker.title("Marker in Sydney");
         mMap.addMarker(marker);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); */
+
+        //Adicionando a marcação d juazeiro
+        LatLng juazeiro = new LatLng(-7.23718, -39.3222);
+        mMap.addMarker(new MarkerOptions()
+                .position(juazeiro)
+                .title("Juazeiro do Norte")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))); //alterar a cor do marcador
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(juazeiro));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(juazeiro, 10));
+        //Mudo de exibição do mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        Toast.makeText(getContext(), "Coordenadas:" + latLng.toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Coordenadas:" + latLng.toString(), Toast.LENGTH_LONG).show();
     }
 }
