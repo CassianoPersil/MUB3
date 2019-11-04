128a3e34fde1265621a986f0f94c1291b6fa083c
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-11-03 21:32:56 -0300

    "Cards do menu Principal"

--- app/src/main/java/com/projetos/mub/RecycleViewAdapter.java
+++ app/src/main/java/com/projetos/mub/RecycleViewAdapter.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,9 +17,9 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
     private Context context;
     private List<Cards> mCard;
 
-    public RecycleViewAdapter(Context context, List<Cards> mCard) {
+    public RecycleViewAdapter(Context context, List<Cards> card) {
         this.context = context;
-        this.mCard = mCard;
+        this.mCard = card;
     }
 
     @NonNull
